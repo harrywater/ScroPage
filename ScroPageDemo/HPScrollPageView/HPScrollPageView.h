e466ac1fa3ed3a9810ef8e1ad138ac912cfcb648
王辉平 <WANGHUIPING032@pingan.com.cn>
2018-11-26 12:32:07 +0800

    组件封装

--- ScroPageDemo/HPScrollPageView/HPScrollPageView.h
+++ ScroPageDemo/HPScrollPageView/HPScrollPageView.h
@@ -1,13 +1,13 @@
 //
-//  ShowScroView.h
+//  HPScrollPageView.h
 //  ScroPageDemo
 //
-//  Created by 王辉平 on 2018/11/23.
+//  Created by 王辉平 on 2018/11/26.
 //  Copyright © 2018年 王辉平. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ShowScroView : UIView
+@interface HPScrollPageView : UIView
 
 @end
---
+++ ScroPageDemo/HPScrollPageView/HPScrollPageView.m
@@ -0,0 +1,21 @@
+//
+//  HPScrollPageView.m
+//  ScroPageDemo
+//
+//  Created by 王辉平 on 2018/11/26.
+//  Copyright © 2018年 王辉平. All rights reserved.
+//
+
+#import "HPScrollPageView.h"
+
+@implementation HPScrollPageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
